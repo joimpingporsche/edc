45bc90227b2c1e8fa25c2b225d7ebfe06392bd3e
Jo Imping <jo.imping@porsche.de>
2026-03-23 10:53:16 +0100

    Erneutes Anpassen des Prompts, explizietes ausschließen von SAFe, TOGAF, LeanIX, BIC -> all_combined4

--- edc/edc_framework.py
+++ edc/edc_framework.py
@@ -453,8 +453,7 @@ class EDC:
     def extract_kg(self, input_text_list: List[str], output_dir: str = None, refinement_iterations=0):
         if output_dir is not None:
             if os.path.exists(output_dir):
-                logger.warning(f"Output directory {output_dir} already exists! Ergebnisse werden überschrieben oder ergänzt.")
-            pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+                pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
             for iteration in range(refinement_iterations + 1):
                 pathlib.Path(f"{output_dir}/iter{iteration}").mkdir(parents=True, exist_ok=True)
 
